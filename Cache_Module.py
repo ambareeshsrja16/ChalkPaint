e96fa72784630913f00c8c421a55ff81eac300af
Ambareesh S N Jayakumari <ambareeshsrja16@gmail.com>
2019-12-03 09:21:35 -0800

    Updated basic functionality

--- Cache_Module.py
+++ Cache_Module.py
@@ -1,7 +1,7 @@
 class Cache:
     def __init__(self,
-                 size_of_cache=100,
-                 caching_policy=1):
+                 size_of_cache=1000,
+                 caching_policy="NON_LRU"):
         """
         :param size_of_cache:
         :param website_server:
@@ -9,111 +9,33 @@ class Cache:
         """
         self.size_of_cache = size_of_cache
         self.caching_policy = caching_policy
-        self.main_cache = URL_Content_Hashmap(max_size=self.size_of_cache)
+        self.main_cache = URL_Content_Hashmap(self.size_of_cache, self.caching_policy)  # TODO : Different one for LRU
 
-    def fetch_content(self, url_request):
+    def __getitem__(self, item):
+        return self.main_cache.__getitem__(item)
+
+    def __setitem__(self, key, value):
+        self.main_cache[key] = value
+
+
+class URL_Content_Hashmap(dict):
+    def __init__(self, max_size, cache_type="NON_LRU"):
         """
-        :param url_request:
-        :return:
-        """
-        # TODO
-        assert isinstance(url_request, str)
-
-        cache_check_inst =  self.check_in_cache(url_request)
-
-        if cache_check_inst.found: # URL is found in cache, returning the content
-            return cache_check_inst.content
-
-        else:
-            # TODO
-            _content = self.request_server(url_request)
-
-            cache_response = self.store_in_cache(url_request, _content)
-
-            if cache_response:
-                # TODO
-                # Maybe note somewhere that this was successfully cached?
-                # TODO IDEA
-                # Use a secondary cache structure for objects/keys cached long ago? This should be slower than your current cache, but faster than reading directly from memory
-                return _content
-            else:
-                #TODO
-                #TODO Document how to deal with this!
-                # TODO Add to test suite
-                print("Not cached! Cache Error")
-
-    def check_in_cache(self, url_request):
-        """
-
-        :param self:
-        :param url_request:
-        :return:
-        """
-        cache_check_inst = Cache_check()
-
-        if url_request in self.main_cache.dictionary:
-            cache_check_inst.found = True
-            cache_check_inst.content = self.main_cache.dictionary[url_request]
-
-        # TODO Add provision for secondary checks and other cache policies here
-
-        return cache_check_inst
-
-    def store_in_cache(self, url_request, content):
-        """
-        :param url_request:
-        :param content:
-        :return:
-        """
-        caching_response = True
-        try:
-            self.main_cache.dictionary[url_request] = content
-        except Exception as e:
-            # TODO
-            # TODO Add to test suite
-            print(f"Cache unsuccessful | Exception raised : {e}")
-            caching_response = False
-
-        return caching_response
-
-    def request_server(self, url_request):
-        """
-        Prototype - DO NOT use for production!
-
-        Source: https://docs.python.org/3/library/http.server.html
-        WARNING: http.server is not recommended for production. It only implements basic security checks.
-
-        :param url_request:
-        :return:
-        """
-
-
-class Cache_check:
-    def __init__(self):
-        self.found = False
-        self.content = None
-
-
-
-class URL_Content_Hashmap:
-    def __init__(self, max_size):
-        """
-
         :param size:
         """
+        super().__init__()
         self.max_size = max_size
-        self.dictionary = dict()
-
-    def __len__(self):
-        """Returns size of Hashmap"""
-        return len(self.dictionary)
+        self.cache_type = cache_type
 
     def is_full(self):
         """
         Returns if the Hashmap has reached full capacity, specified under max_size instance variable
-
         :return: Bool
         """
+        if self.cache_type != "LRU":
+            return True
+
+        # LRU Cache, need to check size
         if len(self) >= self.max_size:
             return True
         else:
@@ -121,7 +43,7 @@ class URL_Content_Hashmap:
 
 
 if __name__ == '__main__':
-    print("Start Proxy Server for Testing")
+    print("Do testing")
     # Initialize test cases and run them here!
     # TODO
 
--- flask_test.py
+++ flask_test.py
@@ -3,38 +3,38 @@ from flask import Flask, request, Response
 import requests
 
 app = Flask(__name__)
+
 cache = Cache(100)
 SITE_NAME = "http://localhost:7000"
 
-@app.route("/<path:path>")  #,  methods=["GET", "POST", "DELETE", "HEAD"]
+
+@app.route("/<path:path>")
 @app.route("/")
 def proxy(path=""):
     global SITE_NAME
-    print(f"Serving at- {SITE_NAME}")
-    if request.method in "GET":
-        print("DEBUG")
-        print("path: ",path)
-        print("url: ",request.url)
-        print("REQ args: ",request.args)
+    print(f"Proxy Serving at - {SITE_NAME}")
 
-        url_bits = request.url.split("?", 1)
-        qs = "?" + url_bits[1] if len(url_bits) > 1 else ""
+    url_bits = request.url.split("?", 1)
+    qs = "?" + url_bits[1] if len(url_bits) > 1 else ""
 
-        host = [h[1] for h in request.headers if h[0].lower() == 'host'][0]
-        recreated_request = "https://"+host+"/"+str(path)+qs
+    host = [h[1] for h in request.headers if h[0].lower() == 'host'][0]
+    recreated_request = "https://" + host + "/" + str(path) + qs
+    cache_key = (recreated_request, request.method)
+
+    if request.method in {"GET", "HEAD"}:
+        if cache_key not in cache.main_cache:
+            # # DEBUG
+            print("NOT FROM CACHE")
+            resp = requests.get(recreated_request, headers=dict(request.headers))
+            response = Response(resp.content, resp.status_code)
+            cache[cache_key] = response
+        return cache.main_cache[cache_key]
+    else:
         resp = requests.get(recreated_request, headers=dict(request.headers))
-        cache_key = (recreated_request, request.method)
-
         response = Response(resp.content, resp.status_code)
         return response
-
-    else:
-        return ""
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=7000)  # Change to a different port so that you needn't run as root
-    # #DEBUG
-    # from pprint import pprint
-    # pprint(vars(request))
-    # print(request.data)
+
